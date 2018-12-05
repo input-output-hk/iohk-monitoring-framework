515760548a6cabd79d4fe312ea391ede7106818d
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2018-12-05 19:56:16 +0200

    [#3] Ease nix.

--- release.nix
+++ release.nix
@@ -1,7 +1,8 @@
 let
-  addRealTimeTestLogs = drv: overrideCabal drv (attrs: {
+  addRealTimeTestLogs = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
     testTarget = "--show-details=streaming";
   });
+
   config = {
     packageOverrides = pkgs: rec {
       haskellPackages = pkgs.haskell.packages.ghc843.override {
@@ -11,6 +12,7 @@ let
       };
     };
   };
+
   nixpkgs = builtins.fetchTarball {
     url    = "https://github.com/NixOS/nixpkgs/archive/069bf7aee30faf7b3ed773cfae2154d761b2d6c2.tar.gz";
     sha256 = "1c44vjb60fw2r8ck8yqwkj1w4288wixi59c6w1vazjixa79mvjvg";
