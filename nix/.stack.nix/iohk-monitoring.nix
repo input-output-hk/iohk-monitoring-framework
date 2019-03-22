2f24d84db612535b28a15c1b3922adf9c04b757d
Alexander Diemand <codieplusplus@apax.net>
2019-03-22 14:24:27 +0100

    [#189] preparing release 0.1.4.0
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- nix/.stack.nix/iohk-monitoring.nix
+++ nix/.stack.nix/iohk-monitoring.nix
@@ -9,7 +9,7 @@
       };
     package = {
       specVersion = "1.10";
-      identifier = { name = "iohk-monitoring"; version = "0.1.3.0"; };
+      identifier = { name = "iohk-monitoring"; version = "0.1.4.0"; };
       license = "MIT";
       copyright = "2018 IOHK";
       maintainer = "";
