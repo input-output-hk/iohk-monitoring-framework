dc60318a01bbb5d966e99971bc0ffed96ba43d24
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-13 12:35:52 +0800

    jailbreak prometheus.

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -30,9 +30,12 @@ let
         # This is similar to jailbreakCabal, however it
         # does not require any messing with cabal files.
         packages.katip.components.library.doExactConfig = true;
+        # lift containers==0.5.* restriction, see
+        # https://github.com/bitnomial/prometheus/commit/61bb7ec834279d6274c9d13b0edcfff3cc42c856
+        packages.prometheus.components.library.doExactConfig = true;
       }
     ];
   };
 
 in
-  pkgSet.config.hsPkgs // { _config = pkgSet.config; }  
+  pkgSet.config.hsPkgs // { _config = pkgSet.config; }
