533f3622a0b6115c3179f1cbae97ded5c6528179
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2019-05-13 14:54:07 +0300

    [#268] TRY Added coveralls coverage.

---
+++ scripts/buildkite/default.nix
@@ -0,0 +1,20 @@
+with import ../../nix/iohk-common.nix;
+with pkgs;
+
+let
+  stack-hpc-coveralls = pkgs.haskell.lib.dontCheck
+    (haskellPackages.callPackage ./stack-hpc-coveralls.nix {});
+
+  stackRebuild = runCommand "stack-rebuild" {} ''
+    ${haskellPackages.ghcWithPackages (ps: [ps.turtle ps.safe ps.transformers])}/bin/ghc -o $out ${./rebuild.hs}
+  '';
+
+  buildTools =
+    [ git nix gnumake stack gnused gnutar coreutils stack-hpc-coveralls ];
+
+in
+  writeScript "stack-rebuild-wrapped" ''
+    #!${stdenv.shell}
+    export PATH=${lib.makeBinPath buildTools}
+    exec ${stackRebuild} "$@"
+  ''
