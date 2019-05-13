2eaedfba869b2ff5b7af9722d18fe9db15232c4f
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-13 11:44:35 +0800

    Add ekg-prometheus-adapter-0.1.0.4 (not part of the lts set)
    ekg-prometheus-adapter does not seem to be part of the lts-13.15
    stackage snapshot.  As such we won't be able to find it.  Adding
    it to the extra-deps should do the trick.

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -5,6 +5,7 @@
         "time-units" = (((hackage.time-units)."1.0.0").revisions).default;
         "ekg" = (((hackage.ekg)."0.4.0.15").revisions).default;
         "ekg-json" = (((hackage.ekg-json)."0.1.0.6").revisions).default;
+        "ekg-prometheus-adapter" = (((hackage.ekg-prometheus-adapter)."0.1.0.4").revisions).default;
         "process" = (((hackage.process)."1.6.5.0").revisions).default;
         "transformers" = (((hackage.transformers)."0.5.6.2").revisions).default;
         } // {
