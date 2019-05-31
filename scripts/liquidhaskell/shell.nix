dd6c73136b335da2aaaef7fcd80561f7175b4e19
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2019-05-31 12:44:29 +0300

    Add contra-tracer in shellFor command.

--- scripts/liquidhaskell/shell.nix
+++ scripts/liquidhaskell/shell.nix
@@ -1,6 +1,6 @@
 with (import ../../default.nix {});
 
 nix-tools._raw.shellFor {
-  packages = ps: with ps; [ iohk-monitoring ];
+  packages = ps: with ps; [ iohk-monitoring contra-tracer ];
   buildInputs = [ nix-tools.exes.liquidhaskell ];
 }
