b99d5053d52a76f5346ed2d6d9541fce80e71b9f
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-03-10 14:20:11 +0300

    CAD-485:  attempt at convenient cross-builds
    nix-build -A haskellPackages.lobemo-examples.components.exes.example-acceptor --argstr target x86_64-windows

--- default.nix
+++ default.nix
@@ -1,20 +1,31 @@
-{ system ? builtins.currentSystem
-, crossSystem ? null
+{ target ? builtins.currentSystem
 # allows to cutomize haskellNix (ghc and profiling, see ./nix/haskell.nix)
 , config ? {}
+, localLib ? import ./lib.nix
 # allows to override dependencies of the project without modifications,
 # eg. to test build against local checkout of iohk-nix:
 # nix build -f default.nix cardano-node --arg sourcesOverride '{
 #   iohk-nix = ../iohk-nix;
 # }'
 , sourcesOverride ? {}
-# pinned version of nixpkgs augmented with overlays (iohk-nix and our packages).
-, pkgs ? import ./nix { inherit system crossSystem config sourcesOverride; }
-, gitrev ? pkgs.iohkNix.commitIdFromGitRepoOrZero ./.git
 }:
+let
+  systemTable = {
+    x86_64-windows = builtins.currentSystem;
+  };
+  crossSystemTable = {
+    x86_64-windows = lib.systems.examples.mingwW64;
+  };
+  system = systemTable.${target} or target;
+  crossSystem = crossSystemTable.${target} or null;
+  pkgs = localLib.iohkNix.getPkgsDefault { inherit system config; };
+  # pkgs ? import ./nix { inherit system crossSystem config sourcesOverride; }
+  inherit (import ./nix { inherit system crossSystem config sourcesOverride; }) commonLib iohkMonitoringHaskellPackages;
+  gitrev = pkgs.iohkNix.commitIdFromGitRepoOrZero ./.git;
+  lib = pkgs.lib;
+in
 with pkgs; with commonLib;
 let
-
   haskellPackages = recRecurseIntoAttrs
     # we are only intersted in listing the project packages:
     (selectProjectPackages iohkMonitoringHaskellPackages);
---
+++ lib.nix
@@ -0,0 +1,30 @@
+let
+  sources = import ./nix/sources.nix;
+  iohkNix = import sources.iohk-nix { sourcesOverride = sources; };
+  # TODO: can we use the filter in iohk-nix instead?
+  cleanSourceFilter = with pkgs.stdenv;
+    name: type: let baseName = baseNameOf (toString name); in ! (
+      # Filter out .git repo
+      (type == "directory" && baseName == ".git") ||
+      # Filter out editor backup / swap files.
+      lib.hasSuffix "~" baseName ||
+      builtins.match "^\\.sw[a-z]$" baseName != null ||
+      builtins.match "^\\..*\\.sw[a-z]$" baseName != null ||
+
+      # Filter out locally generated/downloaded things.
+      baseName == "dist" ||
+      baseName == "node_modules" ||
+
+      # Filter out the files which I'm editing often.
+      lib.hasSuffix ".nix" baseName ||
+      lib.hasSuffix ".dhall" baseName ||
+      lib.hasSuffix ".hs" baseName ||
+      # Filter out nix-build result symlinks
+      (type == "symlink" && lib.hasPrefix "result" baseName)
+    );
+  pkgs = iohkNix.pkgs;
+  lib = pkgs.lib;
+in
+lib // {
+  inherit sources iohkNix pkgs lib cleanSourceFilter;
+}
