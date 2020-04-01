37039a5780806b4c4e7120e2458df50c5e76b8bf
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-04-01 15:36:57 +0300

    CAD-484 snockets:  import trimmed-down snockets code

---
+++ iohk-monitoring/src/Cardano/BM/IOManager.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE CPP        #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- | A shim layer for `Win32-network`'s `IOManager`
+--
+module Cardano.BM.IOManager
+  ( module X
+  ) where
+
+import System.IOManager as X
