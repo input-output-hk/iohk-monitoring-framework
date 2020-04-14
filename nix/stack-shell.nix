47c2ee65ad6a54585d87be8106a0cf635c83aaf4
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-04-14 15:09:40 +0200

    Fix ghc provided by stack-shell.nix
    Should solve the
    "I don't know how to install GHC on your system configuration, please install manually"
    error.

--- nix/stack-shell.nix
+++ nix/stack-shell.nix
@@ -4,5 +4,5 @@ with import ./. {};
 haskell.lib.buildStackProject {
   name = "stack-env";
   buildInputs = with pkgs; [ zlib openssl gmp libffi git systemd haskellPackages.happy ];
-  ghc = (import ../shell.nix {inherit pkgs;}).ghc;
+  ghc = (import ../shell.nix {inherit pkgs;}).ghc.baseGhc;
 }
