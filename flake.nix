b66bdc02f9160d91c64988237df48742f12b824e
Erik de Castro Lopo <erikd@mega-nerd.com>
2025-03-11 10:37:10 +1100

    Nix updates

--- flake.nix
+++ flake.nix
@@ -2,8 +2,21 @@
   description = "iohk-monitoring-framework";
 
   inputs = {
-    haskellNix.url = "github:input-output-hk/haskell.nix";
+
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    hostNixpkgs.follows = "nixpkgs";
+    hackageNix = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+    haskellNix = {
+      # GHC 8.10.7 cross compilation for windows is broken in newer versions of haskell.nix.
+      # Unpin this once we no longer need GHC 8.10.7.
+      url = "github:input-output-hk/haskell.nix/cb139fa956158397aa398186bb32dd26f7318784";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.hackage.follows = "hackageNix";
+    };
+
     iohkNix.url = "github:input-output-hk/iohk-nix";
     incl.url = "github:divnix/incl";
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
